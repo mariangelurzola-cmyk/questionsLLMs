39ab39fa02593d8b22cb6f7b54d117035e5894b4
mariangelurzola-cmyk <mariangel.urzola@udea.edu.co>
2026-04-10 11:08:09 -0500

    Update question-0002-usecase-generator.py

--- myquestions/question-0002-usecase-generator.py
+++ myquestions/question-0002-usecase-generator.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def generar_caso_de_uso():
+    n_filas = 30
+    df = pd.DataFrame({
+        "radiacion_w_m2": np.random.uniform(0, 1100, n_filas),
+        "eficiencia_porcentaje": np.random.uniform(15, 22, n_filas)
 def eficiencia_promedio_alta_radiacion(df_solar):
     pass
 
